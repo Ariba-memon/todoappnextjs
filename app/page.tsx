60bbcf5c105ea638e80aad37b6f03345def78799
Ariba-memon <aribaabdulqadir786@gmail.com>
2023-01-19 00:54:05 +0500

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,91 +1,60 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
-
+"use client"
+import { Button, Flex, ListItem, UnorderedList, Text, Input } from '@chakra-ui/react'
+import { use, useState } from 'react'
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    const [tasks, setTasks] = useState(["1", "2", "3"])
+    const [Item, setItem] = useState("")
+    function removeItem(taskName: string) {
+        setTasks(tasks.filter(task => {
+            return task != taskName
+        }))
+    }
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
+    function AddItem() {
+        if (Item != "" && !tasks.includes(Item)) {
+            let temp = tasks;
+            temp.push(Item)
+            setTasks(temp)
+            setItem("")
+        }
+    }
+    return (
+        <><h1>Todoapp</h1><Flex
+            justifyContent="center"
+            alignItems="center"
+            width="100%"
+            height="100%"
+            flexDirection="column"
 
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
         >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
+            <Input
+                placeholder='Item Name'
+                value={Item}
+                color="black"
+                onChange={(e) => {
+                    setItem(e.target.value)
+                }}>
+            </Input>
+            <Button onClick={AddItem}>Add Item</Button>
+        </Flex> <UnorderedList>
+                {tasks.map((task) => {
+                    return (<ListItem key={task}>
+                        {task}
+                        <Button
+                            ml={10}
+                            backgroundColor="black"
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+                            color="#e7e7e7"
+                            onClick={() => {
+                                removeItem(task)
+                            }}>
+                            Delete
+                        </Button>
+
+
+                    </ListItem>)
+                })}
+            </UnorderedList></>
+    )
 }
